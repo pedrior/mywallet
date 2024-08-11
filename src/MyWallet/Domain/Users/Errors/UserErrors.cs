68d57706361ea356df005e6c0774a775e50c8a93
Pedro Júnior <pedrojdev@gmail.com>
2024-08-11 10:47:03 -0300

    refactor: cleanup unused errors

--- src/MyWallet/Domain/Users/Errors/UserErrors.cs
+++ src/MyWallet/Domain/Users/Errors/UserErrors.cs
@@ -13,12 +13,4 @@ public static class UserErrors
     public static readonly Error CannotChangeToSamePassword = Error.Failure(
         code: "user.cannot_change_to_same_password",
         description: "The new password cannot be the same as the old password.");
-    
-    public static readonly Error CategoryNotFound = Error.NotFound(
-        code: "user.category_not_found",
-        description: "The user does not have the category.");
-    
-    public static readonly Error CategoryAlreadyExists = Error.Conflict(
-        code: "user.category_already_exists",
-        description: "The user already has the category.");
 }
